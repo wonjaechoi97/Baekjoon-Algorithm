339993f271f60dcbbc077dd07506251557099d21
wonjaechoi97 <wj5295@naver.com>
2022-01-19 22:26:11 +0900

    Create Problem2557.java
    첫 백준 알고리즈 문제 풀이

---
+++ baekjoon2558/Problem2557.java
@@ -0,0 +1,13 @@
+package baek;
+
+import java.io.*;
+import java.util.*;
+
+public class Problem2557 {
+
+	public static void main(String[] args) throws Exception{
+		System.out.println("Hello World!");
+
+	}
+
+}

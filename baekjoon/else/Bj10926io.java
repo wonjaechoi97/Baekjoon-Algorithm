800fa1dcd85e5086d5ee1aee008bd43e028732a1
wonjaechoi97 <wj5295@naver.com>
2022-02-01 23:07:15 +0900

    20220201 algorithm solve
    2월 첫째날 1일1커밋 완료

---
+++ baekjoon/else/Bj10926io.java
@@ -0,0 +1,17 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bj10926io {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		String result = s +"??!";
+		
+		System.out.println(result);
+
+	}
+
+}

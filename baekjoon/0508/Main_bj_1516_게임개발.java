459249b34a63efa65b5d3aa7e2511b608208810f
wonjaechoi97 <wj5295@naver.com>
2022-05-08 23:26:44 +0900

    20220508 알고리즘 풀이

---
+++ baekjoon/0508/Main_bj_1516_게임개발.java
@@ -0,0 +1,20 @@
+package p05;
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_1516_게임개발 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N =Integer.parseInt(br.readLine());
+		
+		
+		
+		
+		
+		
+		br.close();
+
+	}
+
+}
